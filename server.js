c0a10613643f737d41e8efc77f915c97f6c5e5f6
Dani <abdulmalikramdani123@gmail.com>
2024-10-12 19:28:48 +0700

    first git

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require("express")
+const { json } = require("express/lib/response")
+const mysql = require("mysql")
+const bodyParser = require("body-parser")
+
+
+const app = express()
+app.set("view engine", "ejs")
+app.set("views", "views")
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "buku_db"
+})
+db.connect((err) => {
+    if (err) throw err
+    console.log("Mysql Connected...")
+
+    app.get("/", (req, res) => {
+        const sql_query = "SELECT * FROM books"
+        db.query(sql_query, (err, result) => {
+            if (err) throw err
+            console.log(result)
+            const books = JSON.parse(JSON.stringify(result))
+            res.render("index", { books: books, title: "Daftar Buku" })
+        })
+
+    })
+    app.get("/delete/:id", (req, res) => {
+
+        const id = req.params.id
+
+        const sql_query = `DELETE FROM books WHERE id = ${id}`
+
+        db.query(sql_query, (err, result) => {
+            if (err) throw err
+            res.redirect("/")
+
+        })
+    })
+    app.post("/tambah", (req, res) => {
+        const judul = req.body.judul
+        const halaman = req.body.halaman
+        const sinopsis = req.body.sinopsis
+        const sql_query = `INSERT INTO books (judul, sinopsis, halaman) VALUES ('${judul}','${sinopsis}','${halaman}')`
+        db.query(sql_query, (err, result) => {
+            if (err) throw err
+            res.send(`
+                <script>
+                  alert('Data berhasil ditambahkan');
+                  window.location.href = "/"
+                </script>
+              `);
+        })
+    })
+    app.listen(8000, () => {
+        console.log("Server is running on port 8000")
+
+
+    })
+
+
+})
